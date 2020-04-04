4f71cb70b2cbb42eaef2792e2d98a1826605d44f
Anthony <atsui4688@gmail.com>
2020-04-04 04:54:24 -0400

    Updates

---
+++ precache-manifest.cb733673da809242ce7de17717c01391.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "546f8328574fa938113bc5997c631553",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f301bd453c33b2031a9e",
+    "url": "/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "eb51bc01717c8b13ad33",
+    "url": "/static/js/2.985071be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.985071be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f301bd453c33b2031a9e",
+    "url": "/static/js/main.59b65468.chunk.js"
+  },
+  {
+    "revision": "fcd8744db4f329d8a96c",
+    "url": "/static/js/runtime-main.ac6f0821.js"
+  }
+]);
